6c267720df7ecc9028b323c66a745d098c1a965a
Peter Jausovec <peterj@microsoft.com>
2017-02-28 15:55:03 -0800

    change 1

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(morgan("dev"));
 
-
-
-
 // application -------------------------------------------------------------
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
