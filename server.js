25a9b8f25ea6e2f4321f0241c16559a304530742
Peter Jausovec <peterj@microsoft.com>
2017-03-23 14:51:41 -0700

    change Thu Mar 23 14:51:41 PDT 2017

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/redirect/routename', function (req, res) {
 });
 
 app.get('/api/ping', function (req, res) {
-    res.send('Version: 99999');
+    res.send('Version: 1');
 });
 // api ------------------------------------------------------------
 app.get('/api', function (req, res) {
