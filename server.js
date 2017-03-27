64ed512ac1506e0cae206d30d1fbfefea1bf9b4a
John Stallo <john.stallo@live.com>
2017-03-27 10:17:43 -0700

    msg

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/redirect/routename', function (req, res) {
 });
 
 app.get('/api/ping', function (req, res) {
-    res.send('Version: JOHN');
+    res.send('Version: PETER');
 });
 // api ------------------------------------------------------------
 app.get('/api', function (req, res) {
